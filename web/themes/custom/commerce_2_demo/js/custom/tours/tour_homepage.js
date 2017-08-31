ecfe84a68fae09e482928e3611cdf9d2a6e76fa9
Hubbs <mhubbard@acromediainc.com>
2017-08-30 17:03:08 -0700

    Start adding various website frontend tours.

---
+++ web/themes/custom/commerce_2_demo/js/custom/tours/tour_homepage.js
@@ -0,0 +1,131 @@
+/**
+ * Homepage tour.
+ */
+
+(function ($, Drupal) {
+
+  // Page 1.
+  function homepageTour(){
+    var homepageTour = introJs();
+
+    // Load defaults.
+    tourDefaults(homepageTour);
+
+    // Tour steps.
+    homepageTour.setOptions({
+      steps: [
+        {
+          // No element makes this tip float on center of page.
+          intro: '<span class="introjs-tooltip__title">Homepage Features Tour</span>' +
+          "This tour will show you all of the unique homepage features packed into our Drupal Commerce 2.x demo site."
+          + '<br><br>' +
+          "Of course, this is just one layout. Drupal's powerful theming engine can adapt to anything your creative staff can imagine."
+        },
+        {
+          element: '.site-header',
+          intro: '<span class="introjs-tooltip__title">Everything Your Customers Need is in the Header</span>' +
+          "The site header is the first thing your customers will see, so it's important that it contains everything customers will need. Branding and navigation aside, let's take a look at some of the more unique elements."
+        },
+        {
+          element: '.site-header__promo',
+          intro: '<span class="introjs-tooltip__title">Top CTA</span>' +
+          "An easily customizable call-to-action at the top of the page is a subtle way to announce promotions."
+        },
+        {
+          element: '.region-language-select',
+          intro: '<span class="introjs-tooltip__title">Multi-store / Multi-language</span>' +
+          "Out of the box, Drupal and Drupal Commerce come with exceptional support for multi-store and multi-language websites."
+        },
+        {
+          element: '.region-header',
+          intro: '<span class="introjs-tooltip__title">Contact Details</span>' +
+          "Let your customers know that help is available."
+        },
+        {
+          element: '.region-header-right',
+          intro: '<span class="introjs-tooltip__title">Customer Access & Quick Cart</span>' +
+          "Customers can login and access their account overview pages via this customer menu. "
+          + '<br><br>' +
+          "A quick-view cart lets them quickly see what they have in their cart at any time."
+        },
+        {
+          element: '.site-header__search',
+          intro: '<span class="introjs-tooltip__title">SOLR Powered Site Search</span>' +
+          "SOLR has become the standard for generating search results. From products or blog posts, Drupal Commerce leverages SOLR heavily."
+        },
+        {
+          element: 'ul.slides',
+          intro: '<span class="introjs-tooltip__title">Rich Media Slider</span>' +
+          "Large, impressive graphics draw the attention of your customers. Rich media sliders are a great way to make in impact."
+        },
+        {
+          element: '.region-above-content',
+          intro: '<span class="introjs-tooltip__title">Content CTAs</span>' +
+          "An assortment of content area CTAs can give customers necessary information or direct them to your more popular products."
+        },
+        {
+          element: '.block-views-blockfeatured-products-block-1',
+          intro: '<span class="introjs-tooltip__title">Featured Products</span>' +
+          "Feature products throughout your website, configured manually or dynamically."
+        },
+        {
+          element: '.block-views-blockfeatured-products-block-3',
+          intro: '<span class="introjs-tooltip__title">Targeted Featured Products</span>' +
+          "Featured products can easily be narrowed down to specific categories, too."
+        },
+        {
+          element: '.block-webform',
+          intro: '<span class="introjs-tooltip__title">Newsletter Sign-up</span>' +
+          "Capture emails for your newsletter and promotional emails. Signups like this can be integrated easily with your existing email delivery provider."
+        },
+        {
+          element: '.region-below-content',
+          intro: '<span class="introjs-tooltip__title">Bottom CTAs</span>' +
+          "Additional CTAs can be provided for unique website features and customer service links."
+        },
+        {
+          element: '.site-footer',
+          intro: '<span class="introjs-tooltip__title">A Place for Everything Else</span>' +
+          "The footer, as always, can be customized to display anything else required on the page. Social media links, contact information, legal jargon, etc."
+        },
+        {
+          // No element makes this tip float on center of page.
+          intro: '<span class="introjs-tooltip__title">Done!</span>' +
+          "As you can see, there are a lot of interesting elements that can make up an ecommerce homepage. Whatever type of homepage layout you would like, Drupal Commerce can adapt! You're only limited by your imagination (and developer resources of course). This completes the Homepage Feature Tour."
+        }
+      ]
+    });
+
+    // Start tour.
+    homepageTour.start();
+  }
+
+
+  //////////////////////////////////////
+  // Start tours from #siteTours modal.
+  //////////////////////////////////////
+
+  // homepageTour.
+  // If not on desired start page, go to that page and append trigger.
+  // Otherwise, close modal and start tour.
+  $('#homepageTour').click(function () {
+    if (window.location.pathname !== '/') {
+      window.location.href = '/?startHomepageTour=true';
+    }
+    else {
+      $('#siteTours').modal('hide');
+      setTimeout(homepageTour, 1000);
+    }
+  });
+
+
+  /////////////////////////////////////////
+  // URL triggers to start/continue tours.
+  /////////////////////////////////////////
+
+  // homepageTour URL triggers.
+  if (RegExp('startHomepageTour', 'gi').test(window.location.search)) {
+    homepageTour();
+  }
+
+})(jQuery, Drupal);
---
+++ web/themes/custom/commerce_2_demo/js/custom/tours/tour_product-page.js
@@ -0,0 +1,122 @@
+/**
+ * Product page tour.
+ */
+
+(function ($, Drupal) {
+
+  // Page 1.
+  function productPageTour(){
+    var productPageTour = introJs();
+
+    // Load defaults.
+    tourDefaults(productPageTour);
+
+    // Override default.
+    productPageTour.setOption('doneLabel', 'Next page');
+
+    // Tour steps.
+    productPageTour.setOptions({
+      steps: [
+        {
+          // No element makes this tip float on center of page.
+          intro: '<span class="introjs-tooltip__title">Product Page Tour</span> ' +
+          'This tour will show you some things you can do with your products. Click > to continue.'
+        },
+        {
+          element: '#block-views-block-featured-products-block-1 .view-content .views-row:first-of-type',
+          intro: '<span class="introjs-tooltip__title">Featured Products</span>' +
+          "Products can easily be configure to show as a featured product. Let's move on to the product page. Click next page to continue."
+        }
+      ]
+    });
+
+    // Start tour and set page/trigger for page 2.
+    productPageTour.start().oncomplete(function() {
+      window.location.href = 'product/custom-mason-jar?productPageTourPage2=true';
+    });
+  }
+
+  // Page 2.
+  function productPageTourPage2(){
+    var productPageTourPage2 = introJs();
+
+    // Load defaults.
+    tourDefaults(productPageTourPage2);
+
+    // Tour steps.
+    productPageTourPage2.setOptions({
+      steps: [
+        {
+          // No element makes this tip float on center of page.
+          intro: '<span class="introjs-tooltip__title">Product Page</span>' +
+          "We're now on the Custom Mason Jar product page. Everything on this page can be customized. Let's take a look at some of the elements configured for this product type."
+        },
+        {
+          element: '.sharethis-inline-share-buttons',
+          intro: '<span class="introjs-tooltip__title">Share Buttons</span>' +
+          'Share buttons let customers quickly share this product with their friends.'
+        },
+        {
+          element: '.product--images',
+          intro: '<span class="introjs-tooltip__title">Product Photos</span>' +
+          'This photo changes as customizable options are selected. It can also be a gallery of image.'
+        },
+        {
+          element: '.product--intro',
+          intro: '<span class="introjs-tooltip__title">Details & Add to Cart</span>' +
+          'Basic details about the product, customization attributes, and an add to cart button are easy to configure per product type.'
+        },
+        {
+          element: '.additional-details',
+          intro: '<span class="introjs-tooltip__title">Additional Information & Review</span>' +
+          'Configure your product pages any way you like. Add more details, reviews and other types of content to inform your customers.'
+        },
+        {
+          element: '.recommended-products',
+          intro: '<span class="introjs-tooltip__title">Recommended Products</span>' +
+          'Configure recommended products to display per product. This type of data can be added to products manually or dynamically.'
+        },
+        {
+          // No element makes this tip float on center of page.
+          intro: '<span class="introjs-tooltip__title">Done!</span>' +
+          "This completes the basic product page tour. Drupal Commerce provides the flexibility to customize your product pages any way you like, from basic to complex. You are in control of your content!"
+        }
+      ]
+    });
+
+    // Start tour.
+    productPageTourPage2.start();
+  }
+
+
+  //////////////////////////////////////
+  // Start tours from #siteTours modal.
+  //////////////////////////////////////
+
+  // productPageTour.
+  // If not on desired start page, go to that page and append trigger.
+  // Otherwise, close modal and start tour.
+  $('#productPageTour').click(function () {
+    if (window.location.pathname !== '/') {
+      window.location.href = '/?startProductPageTour=true';
+    }
+    else {
+      $('#siteTours').modal('hide');
+      setTimeout(productPageTour, 1000);
+    }
+  });
+
+
+  /////////////////////////////////////////
+  // URL triggers to start/continue tours.
+  /////////////////////////////////////////
+
+  // productPageTour URL triggers.
+  if (RegExp('startProductPageTour', 'gi').test(window.location.search)) {
+    productPageTour();
+  }
+  if (RegExp('productPageTourPage2', 'gi').test(window.location.search)) {
+    productPageTourPage2();
+  }
+
+})(jQuery, Drupal);

78c3503ce7f7d9d9f68048b22cf011b246d87c26
Hubbs <mhubbard@acromediainc.com>
2017-10-24 15:03:39 -0700

    Create bin dir if it doesnt exist - Now for the remaining scripts.

--- scripts/remote-drush-commands_plus-uat.sh
+++ scripts/remote-drush-commands_plus-uat.sh
@@ -4,15 +4,19 @@
 # php 7.0 is the default on the server, and drush keeps hitting that instead of
 # the 7.1 that we need.
 
+# $HOME = /home/commerce-2-demo
 
 function main () {
+  # Make sure there is a bin dir in the home directory.
+  test -d $HOME/bin || mkdir $HOME/bin
+
   # Make sure PHP is the version we want.
   test -x $HOME/bin/php && rm $HOME/bin/php
   ln -s /usr/bin/php7.1 $HOME/bin/php
 
   # Make sure Drush is the version we want.
   test -x $HOME/bin/drush && rm $HOME/bin/drush
-  ln -s /home/commerce-2-demo/www/commerceplus/vendor/drush/drush/drush $HOME/bin/drush
+  ln -s $HOME/www/commerceplus/vendor/drush/drush/drush $HOME/bin/drush
 
   # Make sure our versions of Drush and our PHP are the first ones encountered by CLI tools.
   export PATH=$HOME/bin:$PATH
@@ -24,7 +28,7 @@ function main () {
   php -v
 
   # Time to do some work.
-  cd /home/commerce-2-demo/www/commerceplus/web
+  cd $HOME/www/commerceplus/web
   drush updb -y
   drush entup -y
   drush cr

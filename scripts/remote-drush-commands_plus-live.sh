6608f51a0c9e9035b30bdafe7cfda79020ddc2fc
Hubbs <mhubbard@acromediainc.com>
2017-10-24 11:36:48 -0700

    Update gitlab-ci.yml to use remote-drush-commands scripts for all other deploy jobs - base and plus demos.

---
+++ scripts/remote-drush-commands_plus-live.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ue
+
+# We run our drush commands here instead of in the .gitlab-ci.yml file because
+# php 7.0 is the default on the server, and drush keeps hitting that instead of
+# the 7.1 that we need.
+
+
+function main () {
+  # Make sure PHP is the version we want.
+  test -x $HOME/bin/php && rm $HOME/bin/php
+  ln -s /usr/bin/php7.1 $HOME/bin/php
+
+  # Make sure Drush is the version we want.
+  test -x $HOME/bin/drush && rm $HOME/bin/drush
+  ln -s /home/drupalcommerce/www/demoplus/vendor/drush/drush/drush $HOME/bin/drush
+
+  # Make sure our versions of Drush and our PHP are the first ones encountered by CLI tools.
+  export PATH=$HOME/bin:$PATH
+
+  # Some feedback for GitLab jobs
+  which drush
+  drush --version
+  which php
+  php -v
+
+  # Time to do some work.
+  cd /home/drupalcommerce/www/demoplus/web
+  drush updb -y
+  drush entup -y
+  drush cr
+}
+
+# Waiting until now to execute the script ensures that the whole thing arrives before anything gets attempted.
+main "$@"

ccd4475dac2c87c377a6bc46a511e07566de9a1e
Hubbs <mhubbard@acromediainc.com>
2018-03-20 10:48:56 -0700

    Add DrupalCon Preorder page validation for username and stock

--- web/themes/custom/commerce_2_demo/js/custom/plus/uh-drupalcon-preorder.js
+++ web/themes/custom/commerce_2_demo/js/custom/plus/uh-drupalcon-preorder.js
@@ -17,7 +17,21 @@
 
   $('.drupal-username__input').on('validation', function(evt, valid) {
     var addToCartButton = $('.product--full--drupalcon-2018-drupal-commerce-tee input.form-submit');
-    valid ? addToCartButton.removeClass('disabled') : addToCartButton.addClass('disabled');
+    var usernameMessage = $('.drupalcon-preorder__username');
+
+    // Remove disabled class if valid and product is in stock.
+    // Add or remove hidden class to username error message.
+    if (valid) {
+      usernameMessage.addClass('hidden');
+    }
+    else if ( valid && $('.drupalcon-preorder__out-of-stock').hasClass('hidden') ) {
+      usernameMessage.addClass('hidden');
+      addToCartButton.removeClass('disabled');
+    }
+    else {
+      usernameMessage.removeClass('hidden');
+      addToCartButton.addClass('disabled');
+    }
   });
 
 
@@ -29,12 +43,33 @@
 
   Drupal.behaviors.drupalcon = {
     attach: function(context, settings) {
+      var addToCartButton = $('.product--full--drupalcon-2018-drupal-commerce-tee input.form-submit');
+      var $selectedAttribute = $('.drupalcon-preorder-form select > option:selected').once('drupalcon').val();
+      var outOfStockMessage = $('.drupalcon-preorder__out-of-stock');
 
+      // Match selected option with attribute, and set whether in stock or not.
+      $('.drupalcon-preorder-attribute').once('drupalcon').each(function () {
+        var $attributeId = $(this).data('attribute-id').toString();
+        var $stock = $(this).data('stock');
+
+        if (($selectedAttribute === $attributeId) && ($stock === 'out-of-stock')) {
+          outOfStockMessage.removeClass('hidden');
+          addToCartButton.addClass('disabled');
+        }
+      });
+
+      // Remove disabled class if valid has passed and product is in stock.
+      // Add or remove hidden class to username error message.
       if ( $('.drupal-username__form-item').hasClass('has-success') ) {
-        $('.product--full--drupalcon-2018-drupal-commerce-tee input.form-submit').once('drupalcon').removeClass('disabled');
+        $('.drupalcon-preorder__username').addClass('hidden');
+      }
+      else if ( $('.drupal-username__form-item').hasClass('has-success') && $('.drupalcon-preorder__out-of-stock').hasClass('hidden') ) {
+        $('.drupalcon-preorder__username').addClass('hidden');
+        addToCartButton.once('drupalcon').removeClass('disabled');
       }
       else {
-        $('.product--full--drupalcon-2018-drupal-commerce-tee input.form-submit').once('drupalcon').addClass('disabled');
+        $('.drupalcon-preorder__username').removeClass('hidden');
+        addToCartButton.once('drupalcon').addClass('disabled');
       }
     }
   };

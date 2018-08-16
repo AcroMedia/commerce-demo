c1a7a97de88f330399685b0dd233502394455824
Hubbs <mhubbard@acromediainc.com>
2018-08-16 11:29:15 -0700

    Continues adding product add on functionality. Template, styling and js added.

---
+++ web/themes/custom/commerce_2_demo/js/custom/plus/uh-pado.js
@@ -0,0 +1,59 @@
+/**
+ * Commerce Product Add-ons.
+ */
+
+(function ($, Drupal) {
+
+  // Add/remove button functionality.
+  $('div[class*="form-item-add-ons-items-add-ons-"]').each(function () {
+    var thisItem = $(this);
+
+    var button = thisItem.find('.add-on-item__link--add');
+    var checkbox = thisItem.find('input[type="checkbox"]');
+
+    button.click(function (event) {
+      event.preventDefault();
+
+      // Update button text.
+      var buttonText = button.text();
+      $(this).text(buttonText === "Add" ? Drupal.t("Remove") : Drupal.t("Add"));
+
+      // Check or uncheck checkbox.
+      if (checkbox.prop("checked")) {
+        checkbox.prop("checked", false).trigger();
+        thisItem.removeClass('checked')
+      }
+      else {
+        checkbox.prop("checked", true);
+        thisItem.addClass('checked')
+      }
+    })
+  });
+
+  // Check for Add-on fieldsets with variations.
+  if ($("fieldset[id*='edit-add-ons-items-add-ons-']").length) {
+
+    // Loop through each fieldset containing variations.
+    $("fieldset[id*=\'edit-add-ons-items-add-ons-']").each(function () {
+      var thisGroup = $(this);
+
+      // Create toggle button.
+      thisGroup.find('legend .add-on-item__label').after('<a href="#" class="add-on-item__toggle">' + Drupal.t("View options") + '</a>');
+
+      // Use toggle button.
+      thisGroup.find('.add-on-item__toggle').click( function (event) {
+        // Stop default link action.
+        event.preventDefault();
+
+        // Toggle add-on items and active class.
+        thisGroup.find('.fieldset-wrapper').slideToggle();
+        $(this).toggleClass('active');
+
+        // Update button text.
+        var text = $(this).text();
+        $(this).text(text === "View options" ? Drupal.t("Hide options") : Drupal.t("View options"));
+      })
+    });
+  }
+
+})(jQuery, Drupal);

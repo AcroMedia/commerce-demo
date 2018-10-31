d3977327e1d07157f98013df642dd9de374df00c
Hubbs <mhubbard@acromediainc.com>
2018-10-31 13:08:07 -0700

    Fix feature beacon logic and some styling issues.

--- web/themes/custom/commerce_2_demo/js/custom/plus/uh-feature-beacon.js
+++ web/themes/custom/commerce_2_demo/js/custom/plus/uh-feature-beacon.js
@@ -6,6 +6,47 @@
 
   if ($('.feature-beacon__marker').length) {
 
+    // Append markers to elements.
+    $(window).on('load', function () {
+      $('.feature-beacon__marker').each(function () {
+        var $this = $(this);
+
+        // Selector(s) to attach beacon to.
+        var $selector = $this.data('selector');
+
+        // If element exists.
+        if ($($selector).length) {
+          // Append marker.
+          $($selector).append($this);
+
+          // Add available class to marker.
+          // This identifies markers that are attached to elements that exist on the current page.
+          $($selector).find('.feature-beacon__marker').addClass('available');
+
+          // Show or hide marker all markers depending on cookie.
+          if (Cookies.get('hideFeatureBeacons') === 'true') {
+            $('.feature-beacon__marker').hide();
+          }
+          else {
+            $('.feature-beacon__marker.available').show();
+          }
+        }
+      });
+    });
+
+    // Make sure Google Analytics is available. If so, send event data when beacon clicked.
+    $('.feature-beacon__marker').click(function () {
+      if (window.ga && ga.create) {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'Feature Beacon',
+          eventAction: 'Clicked',
+          transport: 'beacon'
+        });
+      }
+    });
+
+    // Hide/show buttons.
     // Usefull variable.
     var $beaconButton = $('.feature-beacon-toggle');
     var $buttonIcon = $beaconButton.find('i');
@@ -24,7 +65,7 @@
     // On button click, toggle beacons and alter button.
     $beaconButton.click(function () {
       if (Cookies.get('hideFeatureBeacons') === 'true') {
-        $('.feature-beacon__marker').fadeIn();
+        $('.feature-beacon__marker.available').fadeIn();
         $buttonIcon.removeClass('fa-star');
         $buttonIcon.addClass('fa-star-o');
         $buttonText.text(Drupal.t('Hide Beacons'));
@@ -36,39 +77,6 @@
         $buttonIcon.addClass('fa-star');
         $buttonText.text(Drupal.t('Show Beacons'));
         Cookies.set('hideFeatureBeacons', 'true');
-      }
-    });
-
-    // Append markers to elements.
-    $(window).on('load', function () {
-      $('.feature-beacon__marker').each(function () {
-        var $this = $(this);
-
-        // Selector(s) to attach beacon to.
-        var $selector = $this.data('selector');
-
-        // Append marker.
-        $($selector).append($this);
-
-        // Show or hide marker all markers depending on cookie.
-        if (Cookies.get('hideFeatureBeacons') === 'true') {
-          $('.feature-beacon__marker').hide();
-        }
-        else {
-          $('.feature-beacon__marker').show();
-        }
-      });
-    });
-
-    // Make sure Google Analytics is available. If so, send event data when beacon clicked.
-    $('.feature-beacon__marker').click(function () {
-      if (window.ga && ga.create) {
-        ga('send', {
-          hitType: 'event',
-          eventCategory: 'Feature Beacon',
-          eventAction: 'Clicked',
-          transport: 'beacon'
-        });
       }
     });
 

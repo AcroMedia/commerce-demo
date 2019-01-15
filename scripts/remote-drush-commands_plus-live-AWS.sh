314462e5b63acdfe6e03e3aa33be3a05cf5e81cb
Hubbs <mhubbard@acromediainc.com>
2019-01-15 10:20:22 -0800

    Update AWS depoy script

--- scripts/remote-drush-commands_plus-live-AWS.sh
+++ scripts/remote-drush-commands_plus-live-AWS.sh
@@ -12,7 +12,7 @@ function main () {
 
   # Make sure PHP is the version we want.
   test -x $HOME/bin/php && rm $HOME/bin/php
-  ln -s /usr/bin/php7.1 $HOME/bin/php
+  ln -s /usr/bin/php7.2 $HOME/bin/php
 
   # Make sure Drush is the version we want.
   test -x $HOME/bin/drush && rm $HOME/bin/drush

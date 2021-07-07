6d802061b14465a9c9d16606210e3e1e12505d74
Alan Abdollahzadeh <aabdollahzadeh@acromedia.com>
2021-07-07 12:05:20 -0700

    Reduced shell scripts to 1 file

--- scripts/remote-drush-commands_deploy.sh
+++ scripts/remote-drush-commands_deploy.sh
@@ -3,8 +3,6 @@
 # We run our drush commands here instead of in the .gitlab-ci.yml file because
 # the default version of php on the server doesn't always match the version we need.
 
-# $HOME = /home/commerce-2-demo
-
 function main () {
   # Make sure there is a bin dir in the home directory.
   test -d $HOME/bin || mkdir $HOME/bin
@@ -15,28 +13,28 @@ function main () {
 
   # Make sure Drush is the version we want.
   test -x $HOME/bin/drush && rm $HOME/bin/drush
-  ln -s $HOME/www/commerce-2-demo/vendor/drush/drush/drush $HOME/bin/drush
+  ln -s $HOME/www/$1/vendor/drush/drush/drush $HOME/bin/drush
 
   # Make sure our versions of Drush and our PHP are the first ones encountered by CLI tools.
   export PATH=$HOME/bin:$PATH
 
-  # Some feedback for GitLab jobs
+  # Some feedback for GitLab jobs.
   which drush
   drush --version
   which php
   php -v
 
   # Update the database.
-  cd $HOME/www/demo/web
+  cd $HOME/www/$2/web
   drush state:set system.maintenance_mode 1 --input-format=integer
   cd ../dumps
   git pull
   cd ..
   db_md5_old=($(cat "database.sql.md5"))
-  db_md5_new=($(md5sum "dumps/demo.database.sql"))
+  db_md5_new=($(md5sum "dumps/$3.database.sql"))
   if [[ "$db_old" != "$db_new" ]]; then
     mv database.sql db_backups/database_$(date +%Y-%m-%d).sql
-    cp dumps/demo.database.sql database.sql
+    cp dumps/$3.database.sql database.sql
     md5sum database.sql
     cd web
     drush sql-drop -y
